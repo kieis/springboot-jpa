21ef96c7d019e966e78302a49e42e78ca6f298d5
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2024-01-16 03:44:45 -0300

    feat: added product controller with post and get default methods

---
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -0,0 +1,55 @@
+package com.example.springboot.controllers;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springboot.dtos.ProductRecordDto;
+import com.example.springboot.models.ProductModel;
+import com.example.springboot.repositories.ProductRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+public class ProductController {
+    @Autowired
+    ProductRepository productRepository; // or with constructors
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(
+        @RequestBody 
+        @Valid 
+        ProductRecordDto productRecordDto
+    ) {
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getAllProducts() {
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+    }
+
+    @GetMapping("/products/${id}")
+    public ResponseEntity<Object> getOneProduct(
+        @PathVariable(value="id") 
+        UUID id
+    ) {
+        Optional<ProductModel> productO = productRepository.findById(id);
+        if(productO.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(productO.get());
+    }
+}
